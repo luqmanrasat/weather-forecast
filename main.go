6e9672aec8424d6a15a4b2e72cfa165ed7825bd7
Luqman Rasat <luqmanrasat@hotmail.com>
2023-11-19 04:01:00 +0800

    add api url

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -13,5 +14,11 @@ func main() {
 		log.Fatalln("Error loading .env file!")
 	}
 
+	openweatherApiEndpoint := os.Getenv("OPENWEATHER_API_ENDPOINT")
 	openweatherApiKey := os.Getenv("OPENWEATHER_API_KEY")
+	lat := os.Getenv("LOCATION_LAT")
+	long := os.Getenv("LOCATION_LONG")
+
+	apiUrl := openweatherApiEndpoint + "?lat=" + lat + "&long=" + long + "&appid=" + openweatherApiKey
+	fmt.Println(apiUrl)
 }
